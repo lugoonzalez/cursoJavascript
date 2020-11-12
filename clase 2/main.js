41096be98afa040dd9dae5faa5ddb99e22ad0052
lugoonzalez <52919240+lugoonzalez@users.noreply.github.com>
2020-11-12 09:48:15 -0300

    minior changes

--- clase 2/main.js
+++ clase 2/main.js
@@ -1,4 +1,4 @@
-var selection = parseInt(prompt("Combo 1: frutillas y peras. Combo 2: arandanos y cerezas. Combo 3: manzanas y kiwi. Ingresa el paquete que quieres comprar:"));
+var selection = parseInt(prompt("Combo 1: frutillas y peras. Combo 2: arándanos y cerezas. Combo 3: manzanas y kiwis. Ingresa el número del combo que quieres comprar:"));
 var neighborhood;
 var shipping; 
 var price; 
@@ -7,15 +7,15 @@ if (selection == 1) {
     // Esto de asignarle el precio a una variable no escala, lo manejaría distinto integrando un backend
     price = 350;
     alert("Elegiste el combo 1 que contiene frutillas y peras.");
-    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitucion, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
+    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitución, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
 } else if (selection == 2) {
     price = 450;
     alert("Elegiste el combo 2 que contiene arandanos y cerezas.");
-    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitucion, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
+    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitución, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
 } else if (selection == 3) {
     price = 550;
     alert("Elegiste el combo 3 que contiene manzanas y kiwi.");
-    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitucion, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
+    neighborhood = prompt("Barrios disponibles para entregas: San Telmo, Barracas, Constitución, Congreso, Monserrat. Ingresa el nombre de tu barrio:");
 } else {
     alert("El combo elegido no está disponible.");
     neighborhood = 0;
